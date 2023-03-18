be47ac3d71b8154a1e882204eb4c4b225fb0ee3e
petrankara <petroskaramitros@gmail.com>
2023-03-18 16:17:34 +0200

    ante na doyme

--- pages/team-dark.js
+++ pages/team-dark.js
@@ -1,8 +1,8 @@
-import InnerSection from "../components/team-dark/innser-section";
+// import InnerSection from "../components/team-dark/innser-section";
 
-export default function TeamDark() {
-	return <InnerSection />;
-}
-export async function getStaticProps() {
-	return { props: { header: "three", footer: "three" } };
-}
+// export default function TeamDark() {
+// 	return <InnerSection />;
+// }
+// export async function getStaticProps() {
+// 	return { props: { header: "three", footer: "three" } };
+// }
--- pages/team.js
+++ pages/team.js
@@ -1,13 +1,13 @@
-import VideoSectionOne from "../components/common/video/video-section-one";
-import BreadcrumbSection from "../components/team/breadcrumb-section";
-import TeamSection from "../components/team/team-section";
+// import VideoSectionOne from "../components/common/video/video-section-one";
+// import BreadcrumbSection from "../components/team/breadcrumb-section";
+// import TeamSection from "../components/team/team-section";
 
-export default function Team() {
-	return (
-		<>
-			<BreadcrumbSection />
-			<VideoSectionOne />
-			<TeamSection />
-		</>
-	);
-}
+// export default function Team() {
+// 	return (
+// 		<>
+// 			<BreadcrumbSection />
+// 			<VideoSectionOne />
+// 			<TeamSection />
+// 		</>
+// 	);
+// }
